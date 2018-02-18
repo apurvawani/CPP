2bba65a297e4aafed93c79e53b5be228a921622e
Apurva Dinesh Wani <apurva.wani06@gmail.com>
2018-02-18 15:30:19 +0530

    Application Of Vectors

--- VariableSizedArrays.cpp
+++ VariableSizedArrays.cpp
@@ -1,6 +1,6 @@
 /*
 Consider an n-element array, a, where each index i in the array contains a reference to an array of k_i integers 
-(where the value of varies from array to array).
+(where the value of k_i varies from array to array).
 Given a, you must answer q queries. Each query is in the format i j, where i denotes an index in array a and j denotes 
 an index in the array located at a[i]. For each query, find and print the value of element j in the array at location 
 a[i] on a new line.
