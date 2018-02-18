6abd963878fffc184390bde5dd1a6ef41f9e092a
Apurva Dinesh Wani <apurva.wani06@gmail.com>
2018-02-18 15:27:36 +0530

    Functions

--- Functions.cpp
+++ Functions.cpp
@@ -1,5 +1,5 @@
 /*
-Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of 
+Write a function max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of 
 them. 
 */
 
